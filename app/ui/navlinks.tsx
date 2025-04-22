29bd0bb8dca0578f9c428763ad1d54a594c51445
hude <qwertyuiop123456@gmail.com>
2025-04-22 09:09:19 +0900

    workpage

--- app/ui/navlinks.tsx
+++ app/ui/navlinks.tsx
@@ -15,11 +15,7 @@ import clsx from 'clsx';
 
 const links = [
   { name: 'Home', href: '/', icon: HomeIcon },
-  {
-    name: 'portfolio',
-    href: '/portfolio',
-    icon: DocumentDuplicateIcon,
-  },
+  {name: 'Portfolio',href: '/portfolio',icon: DocumentDuplicateIcon,},
   { name: 'Works', href: '/works', icon: UserGroupIcon },
   { name: 'Contact', href: '/contact', icon: EnvelopeIcon },
 ];
